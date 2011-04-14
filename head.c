d6fd930e0bb5196fb684ee806320218bad7f1dae
Ali Clark <emailaliclark@gmail.com>
2011-04-14 21:33:53 +0100

    adding head utility

---
+++ head.c
@@ -0,0 +1,82 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "util.h"
+#include "bool.h"
+#include "io.h"
+
+static int files_seen = 0;
+static bool multiple_mode = false;
+
+static void head (char* filename, FILE* strm, void* data)
+{
+    int tmp;
+    int n = *((int*) data);
+    int i = 0;
+
+    UNUSED(filename);
+
+    if (multiple_mode)
+    {
+        if (files_seen++ != 0)
+        {
+            putchar('\n');
+        }
+
+        fputs("==> ",   stdout);
+        fputs(filename, stdout);
+        fputs(" <==\n", stdout);
+    }
+
+    while (true)
+    {
+        tmp = getc(strm);
+
+        if (tmp == EOF)
+        {
+            return;
+        }
+
+        putchar(tmp);
+
+        if (tmp == '\n')
+        {
+            ++i;
+
+            if (i == n)
+            {
+                return;
+            }
+        }
+    }
+}
+
+int main (int argc, char** argv)
+{
+    const char* flags[1] = { NULL };
+    const char* flagstr = "n:";
+    int n = 10;
+
+    (void) input_flags(argc, argv, flagstr, flags);
+
+    if (flags[0] != NULL)
+    {
+        n = atoi(flags[0]);
+
+        if (n <= 0)
+        {
+            n = 1;
+        }
+    }
+
+    if ((optind + 1) < argc)
+    {
+        multiple_mode = true;
+    }
+
+    input_files(argc, argv, head, &n);
+
+    return EXIT_SUCCESS;
+}
